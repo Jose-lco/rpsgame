030dd489c7c798f91a9e951d43cb7c6a5d7511cc
Jose-lco <ndungu642@gmail.com>
2020-11-21 18:24:42 -0800

    captures player name and play

--- js/init.js
+++ js/init.js
@@ -4,5 +4,15 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
+
+
+    $('button').on('click', function(e){
+      e.preventDefault()
+      let playerName = $('input').val();
+      let play = $(this).val();
+      $.get("/shoot" + playerName + play, function(data){
+        console.log(data);
+      })
+    })
   }); // end of document ready
 })(jQuery); // end of jQuery name space
